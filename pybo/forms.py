41638255916d0e4b6e9ac17c9e081403703ce63d
D41JUNG0D <oonja1234@gmail.com>
2022-05-01 17:24:19 +0900

    answer form + print errors

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,17 +1,27 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question  # 사용 모델
         fields = ['subject', 'content']  # 속성
-        widgets = {
+
+        '''widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-        }
+        }'''
 
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer  # 사용 모델
+        fields = ['content']  # 속성
+
+        labels = {
+            'content': '답변내용',
         }
